b815dd8a6c0b0fc286524631594fe859b73febc5
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-05-06 23:02:45 -0500

    20

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configuración para Render con MySQL
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-if DATABASE_URL.startswith("mysql"):
-    DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://")
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
